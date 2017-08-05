6b1fde75c53103135f57468713216ee94c46c4ff
herenkeskin <erenkeskin95@gmail.com>
2017-08-06 00:37:50 +0300

    First Create

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+	
+	To Do List Project
+	Authors:	Hasan Eren Keskin	@erenkeskin 
+				Burak Can Fazla		@burak_can_fazla
+
+	Date: 06.08.2017
+	Description: Yapmamýz gereken, yapmayý istediðimiz ancak unuttuðumuz þeyleri
+				 not alabilebileceðimiz bir konsol uygulamasý.
+
+
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+/* To Do List Projesi */
+int main(void) {
+
+
+	
+	getch();
+	return 0;
+}
